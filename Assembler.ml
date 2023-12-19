c7284724c9d34ebcf018ae848c564cd366181ced
Hubert Gruniaux <hubert@gruniaux.com>
2023-12-19 22:34:11 +0100

    Print errors

--- Assembler.ml
+++ Assembler.ml
@@ -1,3 +1,5 @@
+open Ast
+
 let usage = "usage: asm [options] file.ulm"
 let parse_only = ref false
 
@@ -27,12 +29,31 @@ let lexbuf =
   Lexing.set_filename lexbuf filename;
   lexbuf
 
-let ast = Parser.file Lexer.gen_tokens lexbuf
+let print_error p msg =
+  if p.beg_line = p.end_line then
+    Format.eprintf "\x1b[1mFile \"%s\", line %d, characters %d-%d:\x1b[0m\n" p.file p.beg_line p.beg_col p.end_col
+  else
+    Format.eprintf "\x1b[1mFile \"%s\", lines %d-%d, characters %d-%d:\x1b[0m\n" p.file p.beg_line p.end_line p.beg_col p.end_col;
+  Format.eprintf "\n\x1b[1;31mError\x1b[0m: %s@." msg;
+  exit 1
 
 let () =
-  ignore ast;
-  let a =
-    Ast.{ v = JmpOffset { v = 0l; pos = assert false }; pos = assert false }
-  in
-  let checked_ast = CheckAst.process_pseudo (assert false) a in
-  ignore checked_ast
+  try
+    let ast = Parser.file Lexer.gen_tokens lexbuf in
+    ignore ast;
+    let a =
+      Ast.{ v = JmpOffset { v = 0l; pos = assert false }; pos = assert false }
+    in
+    let checked_ast = CheckAst.process_pseudo (assert false) a in
+    ignore checked_ast
+  with
+  | Lexer.Lexing_error msg ->
+    let s = Lexing.lexeme_start_p lexbuf in
+    let e = Lexing.lexeme_end_p lexbuf in
+    let p = PositionUtils.lexloc_to_pos (s, e) in
+    print_error p msg
+  | Parser.Error ->
+    let s = Lexing.lexeme_start_p lexbuf in
+    let e = Lexing.lexeme_end_p lexbuf in
+    let p = PositionUtils.lexloc_to_pos (s, e) in
+    print_error p "syntax error"
