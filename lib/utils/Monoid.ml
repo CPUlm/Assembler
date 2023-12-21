11b11c9d2f729657e8a9aa30f451eed6933212b8
Gabriel Desfrene <gabriel@desfrene.fr>
2023-12-21 20:36:42 +0100

    Improved Monoid

--- lib/utils/Monoid.ml
+++ lib/utils/Monoid.ml
@@ -14,14 +14,56 @@ let of_elm x = LeafElm x
 let ( @@ ) a b =
   match (a, b) with Empty, t | t, Empty -> t | a, b -> Concat (a, b)
 
-(** Iterate of the set *)
-let rec fold (f : 'acc -> 'a -> 'acc) acc = function
+let rec fold_left f acc = function
   | Empty -> acc
   | LeafL x -> List.fold_left f acc x
   | LeafElm x -> f acc x
   | Concat (left, right) ->
-      let left_acc = fold f acc left in
-      fold f left_acc right
+      let left_acc = fold_left f acc left in
+      fold_left f left_acc right
 
-(** Convert a monoid to a list *)
-let to_list x = fold (fun acc x -> x :: acc) [] x |> List.rev
+let rec fold_right f m acc =
+  match m with
+  | Empty -> acc
+  | LeafElm x -> f x acc
+  | LeafL x -> List.fold_right f x acc
+  | Concat (left, right) ->
+      let right_acc = fold_right f right acc in
+      fold_right f left right_acc
+
+let wrap_f f x =
+  match f x with Either.Left x -> LeafElm x | Either.Right l -> LeafL l
+
+let rec map f = function
+  | Empty -> Empty
+  | LeafElm x -> wrap_f f x
+  | LeafL l -> List.fold_left (fun acc x -> Concat (acc, wrap_f f x)) Empty l
+  | Concat (left, right) -> Concat (map f left, map f right)
+
+let rec mapi index f m =
+  match m with
+  | Empty -> (Empty, index)
+  | LeafElm x -> (wrap_f (f index) x, index + 1)
+  | LeafL l ->
+      List.fold_left
+        (fun (acc, index) x -> (Concat (acc, wrap_f (f index) x), index + 1))
+        (Empty, index) l
+  | Concat (left, right) ->
+      let left_part, index = mapi index f left in
+      let right_part, index = mapi index f right in
+      (Concat (left_part, right_part), index)
+
+let mapi f m = fst (mapi 0 f m)
+let iter f = fold_left (fun () -> f) ()
+
+let iteri f m =
+  let _ =
+    fold_left
+      (fun i x ->
+        f i x;
+        i + 1)
+      0 m
+  in
+  ()
+
+let to_list x = fold_left (fun acc x -> x :: acc) [] x |> List.rev
